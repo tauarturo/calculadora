253aed6cd69c959090ffc826c1f8001af8c65634
García Nuño <agnuno@minsait.com>
2022-06-17 11:45:41 +0200

    Arreglo funciones

--- calculadora/src/main/java/com/example/calculadora/controllers/OperationsController.java
+++ calculadora/src/main/java/com/example/calculadora/controllers/OperationsController.java
@@ -5,6 +5,8 @@ import io.corp.calculator.TracerAPI;
 import io.corp.calculator.TracerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +19,28 @@ public class OperationsController {
 
     TracerImpl tracerImpl = new TracerImpl();
 
+    public static String ERROR="Error de calculo de operacion";
+
     @GetMapping("/suma")
-    public double getSuma(@RequestParam("sumando1") double sumando1, @RequestParam("sumando2") double sumando2) {
-        return sumando1 + sumando2;
+    public String getSuma(@RequestParam("sumando1") String sumando1, @RequestParam("sumando2") String sumando2) {
+        String resultado;
+        try {
+            resultado = String.valueOf(Double.parseDouble(sumando1) + Double.parseDouble(sumando2));
+        }catch (NumberFormatException e) {
+            resultado= ERROR;
+        }
+        tracerImpl.trace(resultado);
+        return resultado;
     }
 
     @GetMapping("/resta")
-    public double getResta(@RequestParam("minuendo") double minuendo, @RequestParam("sustraendo") double sustraendo) {
-        double resultado = minuendo - sustraendo;
+    public String getResta(@RequestParam("minuendo") String minuendo, @RequestParam("sustraendo") String sustraendo) {
+        String resultado;
+        try {
+            resultado = String.valueOf(Double.parseDouble(minuendo) - Double.parseDouble(sustraendo));
+        }catch (NumberFormatException e) {
+            resultado= ERROR;
+        }
         tracerImpl.trace(resultado);
         return resultado;
     }
