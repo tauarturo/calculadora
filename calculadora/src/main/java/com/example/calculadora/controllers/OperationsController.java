4895285d22cf2c6417a8c1536056c4fb5a4644cc
García Nuño <agnuno@minsait.com>
2022-06-17 10:43:15 +0200

    Uso de tracer

--- calculadora/src/main/java/com/example/calculadora/controllers/OperationsController.java
+++ calculadora/src/main/java/com/example/calculadora/controllers/OperationsController.java
@@ -1,6 +1,10 @@
 package com.example.calculadora.controllers;
 
 
+import io.corp.calculator.TracerAPI;
+import io.corp.calculator.TracerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calculadora")
 public class OperationsController {
 
+    TracerImpl tracerImpl = new TracerImpl();
 
     @GetMapping("/suma")
     public double getSuma(@RequestParam("sumando1") double sumando1, @RequestParam("sumando2") double sumando2) {
@@ -20,6 +25,7 @@ public class OperationsController {
     @GetMapping("/resta")
     public double getResta(@RequestParam("minuendo") double minuendo, @RequestParam("sustraendo") double sustraendo) {
         double resultado = minuendo - sustraendo;
+        tracerImpl.trace(resultado);
         return resultado;
     }
 
